511c540e4b5bff89d3dc75286f72bd71cdccb8a9
harshalva <harshith.alva318@gmail.com>
2018-04-14 12:14:37 +0000

    ChatBox

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,5 +1,21 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope) {
-
+    $scope.messages = [
+        {
+            "sender": "USER",
+            "text": "Wassap!"
+        },
+        {
+            "sender": "BOT",
+            "text": "How may I help you?"
+        },
+        {
+            "sender": "USER",
+            "text": "Find nearest petrol bunk"
+        },
+        {
+            "sender": "BOT",
+            "text": "Devegowda!"
+        }];
 });
