d05323cd397c3368078193c5e38449baa461efe1
harshalva <harshith.alva318@gmail.com>
2018-04-15 11:49:22 +0000

    Final

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -3,25 +3,25 @@ var app = angular.module('chatApp', ['ngMaterial']);
 app.config(function ($mdThemingProvider) {
     $mdThemingProvider.theme('default')
         .primaryPalette('cyan')
-        .accentPalette('blue');
+        .accentPalette('cyan');
 });
 
-app.controller('chatController', function ($scope) {
-    $scope.messages = [
-        {
-            "sender": "USER",
-            "text": "Wassap!"
-        },
-        {
-            "sender": "BOT",
-            "text": "How may I help you?"
-        },
-        {
-            "sender": "USER",
-            "text": "Find nearest petrol bunk"
-        },
-        {
-            "sender": "BOT",
-            "text": "Devegowda!"
-        }];
+app.controller('chatController', function ($scope, $sce) {
+    $scope.messages = [];
+    $scope.trust = $sce.trustAsHtml;
+
+    var exampleSocket = new WebSocket("ws://localhost:9000/chatSocket");
+    exampleSocket.onmessage = function (event) {
+        var jsonData = JSON.parse(event.data);
+        jsonData.time = new Date()
+            .toLocaleTimeString();
+        $scope.messages.push(jsonData);
+        $scope.$apply();
+        console.log(jsonData);
+    };
+
+    $scope.sendMessage = function () {
+        exampleSocket.send($scope.userMessage);
+        $scope.userMessage = "";
+    };
 });
