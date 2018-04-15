beb9b92868ba42dd49c6a6a14df90b80b0724bed
harshalva <harshith.alva318@gmail.com>
2018-04-15 07:52:17 +0000

    image

--- app/data/Message.java
+++ app/data/Message.java
@@ -3,6 +3,6 @@ package data;
 public class Message {
 public String text;
 public enum Sender{USER,BOT}
-public FeedResponse feedResponseObject;
+public FeedResponse feedResponse;
 public Sender sender;
 }
